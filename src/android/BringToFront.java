f43ddbe0a461cefa98a5bfa6d01d4d459fffb776
Juan Gaspar Calvo <juangaspar.calvopuga@gmail.com>
2016-09-13 17:21:46 +0200

    Added device wake up device and avoid lock screen features

--- src/android/BringToFront.java
+++ src/android/BringToFront.java
@@ -1,13 +1,17 @@
 package in.lucasdup.bringtofront;
 
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-import android.content.Intent;
+
+import android.app.KeyguardManager;
+import android.app.KeyguardManager.KeyguardLock;
 import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
 import android.util.Log;
 
 /**
@@ -16,18 +20,23 @@ import android.util.Log;
 public class BringToFront extends CordovaPlugin {
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    Log.d("Bring", "action is:" + action);
     if (action.equals("bringToFront")) {
-      Log.d("Bring", "I see you baby");
       Intent notificationIntent = new Intent(cordova.getActivity(), cordova.getActivity().getClass());
       notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
       PendingIntent pendingIntent = PendingIntent.getActivity(cordova.getActivity(), 0, notificationIntent, 0);
-      try 
-      {
+      try {
+        PowerManager powerManager = (PowerManager) cordova.getActivity().getSystemService(Context.POWER_SERVICE);
+        WakeLock wakeLock = powerManager.newWakeLock(
+            (PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP),
+            "TAG");
+        wakeLock.acquire();
+
+        KeyguardManager keyguardManager = (KeyguardManager) cordova.getActivity()
+            .getSystemService(Context.KEYGUARD_SERVICE);
+        KeyguardLock keyguardLock = keyguardManager.newKeyguardLock("TAG");
+        keyguardLock.disableKeyguard();
         pendingIntent.send();
-      }
-      catch (PendingIntent.CanceledException e) 
-      {
+      } catch (PendingIntent.CanceledException e) {
         e.printStackTrace();
       }
       return true;
